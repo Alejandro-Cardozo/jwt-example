a612cedeb640248f382ced6e7d73c2a0b06ce646
Alejandro Cardozo <uri.cardozo@gmail.com>
2020-01-23 17:09:15 -0300

    first route from authController created. password encrypt method created and implemented. web token created.

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,8 +1,26 @@
 const { Router } = require("express");
 const router = Router();
 
-router.post("/signup", (req, res, next) => {
-  res.json("signup");
+const jwt = require("jsonwebtoken");
+const config = require("../config");
+
+const User = require("../models/User");
+
+router.post("/signup", async (req, res, next) => {
+  const { username, email, password } = req.body;
+  const user = new User({
+    username: username,
+    email: email,
+    password: password
+  });
+  user.password = await user.encryptPwd(user.password);
+  await user.save();
+
+  const token = jwt.sign({ id: user._id }, config.secret, {
+    expiresIn: 60 * 60 * 24
+  });
+
+  res.json({ auth: true, token: token });
 });
 
 router.post("/signin", (req, res, next) => {
